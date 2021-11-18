3a0b0f8c10faf95dc0a8a0b54a5333ddd5f9c0e9
Nirajan Pandeycd ..cd cdcd <nirajanp@bu.edu>
2021-11-18 02:24:52 -0500

    :sparkles: completed UI design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
+import {useState} from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 
 // this looks like HTML but it is a JSX: Javascript Syntax Extension
 function App() {
+  const [showAddTask, setShowAddTask] = useState(true)
+  const [tasks, setTask] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food shopping',
+        day: 'Feb 6th at 1:30pm',
+        reminder: false,
+    }
+])
+// Add task
+const addTask = (task) =>  {
+  const id = Math.floor(Math.random() * 1000) + 1
+  const newTask = {id, ...task}
+  setTask([...tasks, newTask])
+}
+
+// Delete task
+const deleteTask = (id) => {
+ setTask(tasks.filter((task)=> task.id !== id))
+}
+
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTask(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+}
   return (
     <div className="container">
-      <Header title={1}/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length > 0 ?  (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No Tasks To Show')}
     </div>
     
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
-const Header = ({title}) => {
+const Header = ({title, onAdd, showAdd}) => {
+    
     return (
-      <header>
+      <header className='header'>
         <h1>{title}</h1>
+        <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
       </header>
     )
 }
@@ -17,6 +20,18 @@ Header.defaultProps ={
 // way to make code more robust by specifying types
 Header.propTypes = {
     title: PropTypes.string,
-  }
+}
+
+
+// To use inline css do this inside the tag 
+// style={{color: 'red', backgroundColor:'black',}}
+
+// this is for adding css 
+// you will have to include this in tag you want this
+// css to be implemented style={headingStyle}
+// const headingStyle = {
+//     color: 'red', 
+//     backgroundColor:'black',
+// }
 
 export default Header
